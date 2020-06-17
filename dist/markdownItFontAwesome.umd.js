28dfe256ba851e0c06297e514baf571215d0cd2e
Ger Hobbelt <ger@hobbelt.com>
2020-06-17 21:13:19 +0200

    bumped build revision

--- dist/markdownItFontAwesome.umd.js
+++ dist/markdownItFontAwesome.umd.js
@@ -1,4 +1,4 @@
-/*! markdown-it-fontawesome 0.3.0-3 https://github.com//GerHobbelt/markdown-it-fontawesome @license MIT */
+/*! markdown-it-fontawesome 0.3.0-4 https://github.com//GerHobbelt/markdown-it-fontawesome @license MIT */
 
 (function (factory) {
   typeof define === 'function' && define.amd ? define(factory) :
